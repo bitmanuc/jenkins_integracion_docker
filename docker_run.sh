9e720b096a071410305a518d50eed7a807ee6fdc
bitmanuc <31716953+bitmanuc@users.noreply.github.com>
2020-05-21 09:30:27 -0500

    Control de errores
    Se activa control de errores basico

--- docker_run.sh
+++ docker_run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 mkdir -p build/
-#if [ $? != 0 ]; then
-#  exit 1
-#fi
+if [ $? != 0 ]; then
+  exit 1
+fi
 
 docker run node:7-alpine node --version
 docker run node:6-alpine node --version
 docker run -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/apps/ maven:3-alpine ls -la /apps
 
 mkdirs build/
-#if [ $? != 0 ]; then
-#  exit 1
-#fi
-
+if [ $? != 0 ]; then
+  exit 1
+fi
